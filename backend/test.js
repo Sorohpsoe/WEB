9486364ab92dfcc60e2fc73086cbb69be2a9732f
Sorohpsoe <matteo.devergne@gmail.com>
2024-03-20 17:29:35 +0100

    Update MongoDB connection string and add function to create new document in Viandes collection

--- backend/test.js
+++ backend/test.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://baptrgtt:password@steakgenerator.aqcbsvs.mongodb.net/STEAK_GENERATOR_DB?retryWrites=true&w=majority&appName=SteakGenerator', { useNewUrlParser: true, useUnifiedTopology: true })  .then(() => console.log('Connexion à MongoDB réussie !'))
+mongoose.connect('mongodb+srv://baptrgtt:paslemotdepasse@steakgenerator.aqcbsvs.mongodb.net/STEAK_GENERATOR_DB?retryWrites=true&w=majority&appName=SteakGenerator', { useNewUrlParser: true, useUnifiedTopology: true })  .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Définir le schéma pour la collection "Viandes"
@@ -41,8 +41,27 @@ const Facture = mongoose.model('Facture', FactureSchema);
 
 const Client = mongoose.model('Client', ClientSchema);
 
-// Créer un nouveau document dans la collection "Viandes"
-const viande = new Viande({ nom: 'Boeuf', quantite: 5, description: 'Viande de boeuf de qualité supérieure', prix: 10 });
+
+async function addViande(new_nom, new_quantite, new_poids, new_description, new_prix, new_prix_kilo) {
+  try {
+    let viande;
+    if (new_prix && new_quantite) {
+      viande = new Viande({ nom: new_nom, quantite: new_quantite, description: new_description, prix: new_prix });
+    } else if (new_prix_kilo && new_poids) {
+      viande = new Viande({ nom: new_nom, poids: new_poids, description: new_description, prix_kilo: new_prix_kilo });
+    } else {
+      throw new Error('Invalid parameters');
+    }
+    await viande.save();
+    console.log('Document créé avec succès !');
+  } catch (err) {
+    console.log('Erreur lors de la création du document : ', err);
+  }
+}
+
+
+
+//addViande('Poulet', 10, null, 'Viande de poulet de qualité supérieure', 5, null);
 
 /*
 // Sauvegarder le document dans la collection "Viandes"
@@ -61,12 +80,16 @@ viande.save()
     .catch((err) => console.log('Erreur lors de la suppression des documents :', err));
 
 */
+
+/*
 // Rajouter +1 à la quantité de viande de type "Boeuf"
 Viande.updateOne({ type: 'Boeuf' }, { $inc: { quantite: 1 } })
   .then(() => {
     console.log('Quantité de viande de type "Boeuf" mise à jour avec succès !');
   })
   .catch((err) => console.log('Erreur lors de la mise à jour de la quantité de viande :', err));
+*/
+
 
 // Rechercher tous les documents dans la collection "Viandes"
 Viande.find()
