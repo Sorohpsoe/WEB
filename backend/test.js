6b6925a2bdbe1179442e4cb1e27db6844af95a8d
Sorohpsoe <matteo.devergne@gmail.com>
2024-03-20 09:42:38 +0100

    Update MongoDB connection in test.js

--- backend/test.js
+++ backend/test.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = 'mongodb+srv://baptrgtt:password@steakgenerator.aqcbsvs.mongodb.net/?retryWrites=true&w=majority&appName=SteakGenerator';
+const uri = 'mongodb+srv://baptrgtt:WYk7zg1W9LZeRk2G@steakgenerator.aqcbsvs.mongodb.net/?retryWrites=true&w=majority&appName=SteakGenerator';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  if (err) {
-    console.log('Connexion à MongoDB échouée !', err);
-  } else {
-    console.log('Connexion à MongoDB réussie !');
-    client.close();
-  }
+console.log('Tentative de connexion à MongoDB...');
+
+client.connect().then(() => {
+  console.log('Connexion à MongoDB réussie !');
+  client.close();
+}).catch(err => {
+  console.log('Connexion à MongoDB échouée !', err);
 });
