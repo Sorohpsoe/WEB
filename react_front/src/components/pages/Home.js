41453c64e052460e6c8beaac99ecf424cecb9b3b
Vincent Fromont <vincent.fromont@protonmail.com>
2024-03-27 11:26:28 +0100

    prettier

--- react_front/src/components/pages/Home.js
+++ react_front/src/components/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Banner from '../Banner'
-
+import Banner from "../Banner";
 
 function Home() {
   return (
     <div>
-      <Banner /> 
-     
+      <Banner />
     </div>
   );
 }
