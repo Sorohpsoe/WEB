8d49a12512f81899c91b1b0d8bab0591ca9845f5
Sorohpsoe <matteo.devergne@gmail.com>
2024-03-27 09:49:41 +0100

    Refactor Viande and Facture schemas, add new fields

--- backend/test.js
+++ backend/test.js
@@ -3,33 +3,26 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://baptrgtt:paslemotdepasse@steakgenerator.aqcbsvs.mongodb.net/STEAK_GENERATOR_DB?retryWrites=true&w=majority&appName=SteakGenerator', { useNewUrlParser: true, useUnifiedTopology: true })  .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+
 // Définir le schéma pour la collection "Viandes"
 const ViandeSchema = new mongoose.Schema({
   nom: String,
-  quantite: Number,
+  cat1 : String,
+  cat2 : String,
   poids: Number,
+  prix_kilo: Number,
   description: String,
-  prix: Number,
-  prix_kilo: Number
 });
 
 const FactureSchema = new mongoose.Schema({
-  id_facture: Number,
-  id_client: Number,
-  date: Date,
-  liste_produits_quantite: {},
-  prix_total: Number,
-  etat: String
-});
-
-const ClientSchema = new mongoose.Schema({
-  id_client: Number,
   nom: String,
   prenom: String,
-  mdp : String,
   adresse: String,
   email: String,
   telephone: String,
+  date: Date,
+  liste_produits_quantite: {},
+  prix_total: Number,
   notes: String
 });
 
@@ -39,29 +32,60 @@ const Viande = mongoose.model('Viande', ViandeSchema);
 
 const Facture = mongoose.model('Facture', FactureSchema);
 
-const Client = mongoose.model('Client', ClientSchema);
+;
 
 
-async function addViande(new_nom, new_quantite, new_poids, new_description, new_prix, new_prix_kilo) {
+async function addViande(new_nom,new_cat1, new_cat2, new_poids, new_prix_kilo, new_description) {
   try {
     let viande;
-    if (new_prix && new_quantite) {
-      viande = new Viande({ nom: new_nom, quantite: new_quantite, description: new_description, prix: new_prix });
-    } else if (new_prix_kilo && new_poids) {
-      viande = new Viande({ nom: new_nom, poids: new_poids, description: new_description, prix_kilo: new_prix_kilo });
-    } else {
-      throw new Error('Invalid parameters');
-    }
+    viande = new Viande({
+      nom: new_nom,
+      cat1: new_cat1,
+      cat2: new_cat2,
+      poids: new_poids,
+      prix_kilo: new_prix_kilo,
+      description: new_description
+    });
+
+
     await viande.save();
-    console.log('Document créé avec succès !');
+
+    console.log('Viande créé avec succès !');
+
   } catch (err) {
-    console.log('Erreur lors de la création du document : ', err);
+    console.log('Erreur lors de la création de la viande : ', err);
   }
 }
 
 
+async function addFacture(new_nom,new_prenom,new_adresse,new_email,new_telephone,new_date,new_liste,new_total,new_notes) {
+  try {
+    let facture;
 
-//addViande('Poulet', 10, null, 'Viande de poulet de qualité supérieure', 5, null);
+    facture = new Facture({
+      nom: new_nom,
+      prenom: new_prenom,
+      adresse: new_adresse,
+      email: new_email,
+      telephone: new_telephone,
+      date: new_date,
+      liste_produits_quantite: new_liste,
+      prix_total: new_total,
+      notes: new_notes
+    });
+
+    await facture.save();
+
+    console.log('Facture créé avec succès !');
+    
+  } catch (err) {
+    console.log('Erreur lors de la création de la facture : ', err);
+  }
+}
+
+
+//addViande('Poulet', 10, 'Viande de poulet de qualité supérieure', 5);
+//addFacture('John', 'Doe', '123 Main St', 'john@example.com', '555-1234', new Date(), { id_steak: 2, id_cotelette: 3 }, 50, 'Some notes');
 
 /*
 // Sauvegarder le document dans la collection "Viandes"
