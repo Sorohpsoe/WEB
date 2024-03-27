ea20900082adfaca4079529edf3b68aa63a90878
theofeur <theo.archaud@insa-lyon.fr>
2024-03-27 16:59:45 +0100

    ajout footer

--- react_front/src/components/Banner.js
+++ react_front/src/components/Banner.js
@@ -3,10 +3,17 @@ import logo from '../assets/logo.png'
 
 function Banner() {
     return (
+        <div>
         <div className='banner'>
             <img src={logo} alt="logo de la ferme de Recornet" className='banner-logo' />
             <h1 className='banner-title'>La ferme de Recornet</h1>
         </div>
+        <div>
+        <footer className="footer">
+          <p>© 2024 La ferme de Recornet. Tous droits réservés.</p>
+        </footer>
+      </div>
+      </div>
     )
 }
 
