840a93dbdcd073ce990b1b36d721fd8be2b4fdec
Skyfiv3 <baptrgtt@gmail.com>
2024-03-27 17:58:59 +0100

    Mise à jour des routes de récupération et de mise à jour des produits

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ var CONNECTION_STRING="mongodb+srv://baptrgtt:WYk7zg1W9LZeRk2G@steakgenerator.aq
 var DATABASE_NAME = "STEAK_GENERATOR_DB";
 var database;
 
+
+
 app.listen(5038, () =>{
     Mongoclient.connect(CONNECTION_STRING,(error, client) => {
         database = client.db(DATABASE_NAME);
@@ -20,13 +22,32 @@ app.listen(5038, () =>{
     })
 
 })
-
-app.get("/api/app/Viandes", (request, response) => {
-
-    database.collection("items").find({}).toArray((error, result) => {
-        response.send(result);  
+// Route pour obtenir un document spécifique en fonction de l'ID
+app.get('/api/app/Viandes/:id', (request, response) => {
+    const produitId = request.params.id;
+    const int_id = parseInt(produitId);
+  
+    database.collection('items').findOne({ id: int_id }, (error, result) => {
+      if (error) {
+        console.error('Erreur:', error);
+        response.status(500).json({ error: 'Une erreur est survenue lors de la récupération du produit.' });
+      } else {
+        response.json(result);
+      }
     });
-});
+  });
+  
+  // Route pour obtenir tous les documents
+  app.get('/api/app/Viandes', (request, response) => {
+    database.collection('items').find().toArray((error, result) => {
+      if (error) {
+        console.error('Erreur:', error);
+        response.status(500).json({ error: 'Une erreur est survenue lors de la récupération des produits.' });
+      } else {
+        response.json(result);
+      }
+    });
+  });
 
 app.post("/api/app/AddViandes",multer().none(), (request, response) => {
     database.collection("items").count({},function(error, numOfDocs)
@@ -43,7 +64,27 @@ app.post("/api/app/AddViandes",multer().none(), (request, response) => {
 
 app.delete("/api/app/DeleteViandes", (request, response) => {
     database.collection("items").deleteOne({
-        id: request.query._id
+        id: request.query.id
     });
     response.json("Item deleted successfully");
 }) 
+
+
+
+app.put('/api/app/Viandes', (request, response) => {
+  const produitId = request.query.id;
+  const newStock = request.body.quantite;
+
+  database.collection('items').updateOne(
+    { id: produitId },
+    { $set: { quantite: newStock } },
+    (error, result) => {
+      if (error) {
+        console.error('Erreur:', error);
+        response.status(500).json({ error: 'Une erreur est survenue lors de la mise à jour du produit.' });
+      } else {
+        response.json({ message: 'Produit mis à jour avec succès.' });
+      }
+    }
+  );
+});
