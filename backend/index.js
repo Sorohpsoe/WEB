74063c168e3fafb96abad027e9a3d62814fd765d
Skyfiv3 <baptrgtt@gmail.com>
2024-03-29 12:30:47 +0100

    Mise à jour des fonctionnalités d'ajout, de modification et de suppression des produits

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,10 @@ app.post("/api/app/AddViandes",multer().none(), (request, response) => {
     {
         database.collection("items").insertOne({
             id: (numOfDocs+1).toString(),
-            description: request.body.newDescription,
+            nom: request.body.nom,
+            description: request.body.description,
+            poids: request.body.poids,
+            quantite: request.body.quantite,
             prix: request.body.prix
         });
         response.json("Item added successfully");
@@ -71,13 +74,15 @@ app.delete("/api/app/DeleteViandes", (request, response) => {
 
 
 
-app.put('/api/app/Viandes', (request, response) => {
+app.put('/api/app/addViandes', (request, response) => {
   const produitId = request.query.id;
   const newStock = request.body.quantite;
+  const int_stock = parseInt(newStock);
+  const int_id = parseInt(produitId);
 
   database.collection('items').updateOne(
-    { id: produitId },
-    { $set: { quantite: newStock } },
+    { id: int_id },
+    { $set: { quantite: int_stock } },
     (error, result) => {
       if (error) {
         console.error('Erreur:', error);
@@ -87,4 +92,59 @@ app.put('/api/app/Viandes', (request, response) => {
       }
     }
   );
+});
+
+app.put('/api/app/addViandes/:id', (request, response) => {
+  const produitId = request.params.id;
+  const produit = request.body;
+  const int_id = parseInt(produitId);
+  console.log("produit", produit);
+
+  database.collection('items').updateOne(
+    { id: int_id },
+    { $set: produit },
+    (error, result) => {
+      if (error) {
+        console.error('Erreur:', error);
+        response.status(500).json({ error: 'Une erreur est survenue lors de la mise à jour du produit.' });
+      } else if (result.matchedCount === 0) {
+        response.status(404).json({ message: 'Produit non trouvé.' });
+      } else {
+        response.json({ message: 'Produit mis à jour avec succès.' });
+      }
+    }
+  );
+});
+
+
+app.delete('/api/app/deleteViandes/:id', (request, response) => {
+  const produitId = request.params.id;
+  const int_id = parseInt(produitId);
+  console.log(" ID a supprimer: ", int_id);
+
+  database.collection('items').deleteOne(
+    { id: int_id },
+    (error, result) => {
+      if (error) {
+        console.error('Erreur:', error);
+        response.status(500).json({ error: 'Une erreur est survenue lors de la suppression du produit.' });
+      } else {
+        response.json({ message: 'Produit supprimé avec succès.' });
+      }
+    }
+  );
+});
+
+
+app.post('/api/app/AddViandes/:id', (request, response) => {
+  const produit = request.body;
+
+  database.collection('items').insertOne(produit, (error, result) => {
+    if (error) {
+      console.error('Erreur:', error);
+      response.status(500).json({ error: 'Une erreur est survenue lors de la création du produit.' });
+    } else {
+      response.json({ message: 'Produit créé avec succès.', id: result.insertedId });
+    }
+  });
 });
